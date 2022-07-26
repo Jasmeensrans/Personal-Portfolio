051bd434be2b603d9f07402d412ef1e6b817c655
Jasmeen Sran <jasmeen.sran@mail.utoronto.ca>
2022-07-26 18:31:53 -0400

    favicon

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import { Navbar } from "./components/common/Navbar";
 import Experience from "./components/experience/Experience";
 import Landing from "./components/landing/Landing";
 import Skills from "./components/skills/Skills";
+import Favicon from 'react-favicon'
 
 function App() {
   return (
     <div>
       <CodeLines />
+      <Favicon url="https://img.icons8.com/external-victoruler-flat-victoruler/344/external-girl-people-victoruler-flat-victoruler-1.png"></Favicon>
       <Navbar />
       <Landing />
       <Skills />

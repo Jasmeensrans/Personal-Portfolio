803c1855a3816bf866914d2b4361e367ec786a2b
Jasmeen Sran <jasmeen.sran@mail.utoronto.ca>
2022-07-28 17:28:03 -0400

    design refinement

--- src/components/experience/CourseCard.tsx
+++ src/components/experience/CourseCard.tsx
@@ -10,7 +10,7 @@ export default function CourseCard(props: {
     <>
       <div className="course-container" onClick={()=> 
         // eslint-disable-next-line no-restricted-globals
-        location.href = props.link}>
+        location.href = props.link} style={{width: '155px', height: '250px'}}>
         <div style={{height: '150px', width: '130px', padding: '10px'}}>
             <img src={props.pic} style={{width: '100%', height: '100%'}}alt=""/>
         </div>
